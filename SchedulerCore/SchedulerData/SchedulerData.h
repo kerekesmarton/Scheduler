ba274400a922ba7a9c963cbe8c82d16515c31e87
Kerekes Marton <kerekes.j.marton@gmail.com>
2020-02-10 10:27:34 +0100

    Folder structure and dependency tree

---
+++ SchedulerCore/SchedulerData/SchedulerData.h
@@ -0,0 +1,19 @@
+//
+//  SchedulerData.h
+//  SchedulerData
+//
+//  Created by Marton Kerekes on 09/02/2020.
+//  Copyright © 2020 Marton Kerekes. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SchedulerData.
+FOUNDATION_EXPORT double SchedulerDataVersionNumber;
+
+//! Project version string for SchedulerData.
+FOUNDATION_EXPORT const unsigned char SchedulerDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SchedulerData/PublicHeader.h>
+
+
---
+++ SchedulerCore/SchedulerDomain/SchedulerDomain.h
@@ -0,0 +1,19 @@
+//
+//  SchedulerDomain.h
+//  SchedulerDomain
+//
+//  Created by Marton Kerekes on 09/02/2020.
+//  Copyright © 2020 Marton Kerekes. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SchedulerDomain.
+FOUNDATION_EXPORT double SchedulerDomainVersionNumber;
+
+//! Project version string for SchedulerDomain.
+FOUNDATION_EXPORT const unsigned char SchedulerDomainVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SchedulerDomain/PublicHeader.h>
+
+
